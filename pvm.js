bbcc5437209cb726bd41a337ef126e3653f3a96d
KalybosPro <kalyboskokou1@gmail.com>
2025-06-28 00:37:54 +0100

    Fix: uncomment shebang for Node execution

--- pvm.js
+++ pvm.js
@@ -1,6 +1,6 @@
-// pvm.js - PHP Version Manager CLI (global)
+#!/usr/bin/env node
 
-//  #!/usr/bin/env node
+// pvm.js - PHP Version Manager CLI (global)
 
 const fs = require('fs');
 const path = require('path');
